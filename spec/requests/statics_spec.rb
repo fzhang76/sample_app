b8e6e49d1fa0719dfb03c69b754ebd630818d8e1
Fan Zhang <fan.zhang@rivalry.com>
2014-02-09 15:19:57 -0500

    Finish static pages

---
+++ spec/requests/statics_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Statics Pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    end
+
+    it "should have the title 'Home'" do
+	    visit '/static_pages/home'
+	    expect(page).to have_title("Fan Zhang | Home")
+    end
+  end
+
+describe "Help page" do
+
+	it "should have the content 'Help'" do 
+		visit '/static_pages/help'
+		expect(page).to have_content('Help')
+  end
+
+	    it "should have the title 'Help'" do
+	    visit '/static_pages/help'
+	    expect(page).to have_title("Fan Zhang | Help")
+    end
+ end
+
+describe "About page" do
+
+	it "should have the content 'About Us'" do
+		visit '/static_pages/about'
+		expect(page).to have_content('About Us')
+	end
+
+	    it "should have the title 'About Us'" do
+	    visit '/static_pages/about'
+	    expect(page).to have_title("Fan Zhang | About Us")
+            end
+end
+end
