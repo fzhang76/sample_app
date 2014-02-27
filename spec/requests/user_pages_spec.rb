977da9c1553f491ee0f11be31f0b1bb38d98434d
Fan Zhang <fan.zhang@rivalry.com>
2014-02-26 23:31:22 -0500

    Finish layout and routs 2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
-describe "User Pages" do
-	subject { page }  
-	
-	describe "signup page" do
-		before { visit signup_path }
-		
-		it { should have_content('Sign up') }
-		it { should have_title(full_title('Sign up')) }
-end
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 end
